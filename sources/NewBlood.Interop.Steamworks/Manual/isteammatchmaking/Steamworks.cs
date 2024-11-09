fb54504ebca43bf65a36d18082bbe996f8a78e49
Benjamin Moir <DaZombieKiller@users.noreply.github.com>
2024-11-09 17:21:57 +1000

    Change BChatMemberStateChangeRemoved return type to bool

--- sources/NewBlood.Interop.Steamworks/Manual/isteammatchmaking/Steamworks.cs
+++ sources/NewBlood.Interop.Steamworks/Manual/isteammatchmaking/Steamworks.cs
@@ -7,8 +7,8 @@ public static partial class Steamworks
     [NativeTypeName("const int")]
     public const int HSERVERQUERY_INVALID = unchecked((int)0xFFFFFFFF);
 
-    public static byte BChatMemberStateChangeRemoved(EChatMemberStateChange rgfChatMemberStateChangeFlags)
+    public static bool BChatMemberStateChangeRemoved(EChatMemberStateChange rgfChatMemberStateChangeFlags)
     {
-        return (rgfChatMemberStateChangeFlags & (k_EChatMemberStateChangeDisconnected | k_EChatMemberStateChangeLeft | k_EChatMemberStateChangeKicked | k_EChatMemberStateChangeBanned)) != 0 ? (byte)1 : (byte)0;
+        return (rgfChatMemberStateChangeFlags & (k_EChatMemberStateChangeDisconnected | k_EChatMemberStateChangeLeft | k_EChatMemberStateChangeKicked | k_EChatMemberStateChangeBanned)) != 0;
     }
 }
